5ef548f9aae8880a7041c0998526ad8acfe67bda
adjierosyidin48 <adjierosyidin48@gmail.com>
2020-06-08 16:29:32 +0700

    first commit

---
+++ lib/maps.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MyMap extends StatefulWidget {
+  @override
+  State<MyMap> createState() => MyMapSampleState();
+}
+
+final Map<String, Marker> _markers = {};
+
+class MyMapSampleState extends State<MyMap> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(title: Text('Tags Map')),
+      body: GoogleMap(
+        scrollGesturesEnabled: true,
+        tiltGesturesEnabled: true,
+        rotateGesturesEnabled: true,
+        myLocationEnabled: true,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(40.688841, -74.044015),
+          zoom: 11,
+        ),
+        markers: _markers.values.toSet(),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _getLocation() async {
+    var currentLocation = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+
+    setState(() {
+      _markers.clear();
+      final marker = Marker(
+        markerId: MarkerId("curr_loc"),
+        position: LatLng(currentLocation.latitude, currentLocation.longitude),
+        infoWindow: InfoWindow(title: 'Your Location'),
+      );
+      _markers["Current Location"] = marker;
+    });
+  }
+}
